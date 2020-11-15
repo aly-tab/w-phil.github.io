6cf4a7f202424faf5b02effe622b239f1f6d8978
w-phil <71557538+w-phil@users.noreply.github.com>
2020-11-15 12:55:25 -0500

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var app = document.getElementById('app');
-
-var typewriter = new Typewriter(app, {
-    delay: 150
-});
-
-typewriter.typeString('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque quis efficitur nibh.')
-.start();
-
